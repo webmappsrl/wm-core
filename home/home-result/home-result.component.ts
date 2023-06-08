7c6f6116136bd9d88c073192aa9d5d166beb86fd
bongiu <peppedeka@gmail.com>
2023-06-08 12:59:30 +0200

    ...

--- home/home-result/home-result.component.ts
+++ home/home-result/home-result.component.ts
@@ -5,6 +5,8 @@ import {
   Output,
   ViewEncapsulation,
   EventEmitter,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 
@@ -15,14 +17,22 @@ import {BehaviorSubject} from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class WmHomeResultComponent {
-  showResultType$: BehaviorSubject<string> = new BehaviorSubject<string>('pois');
-  @Input() tracks: IHIT[];
+export class WmHomeResultComponent implements OnChanges {
   @Input() loadingTracks: boolean;
   @Input() pois: any[];
-  @Output() trackEVT: EventEmitter<number> = new EventEmitter();
+  @Input() tracks: IHIT[];
   @Output() poiEVT: EventEmitter<any> = new EventEmitter();
+  @Output() trackEVT: EventEmitter<number> = new EventEmitter();
+
+  showResultType$: BehaviorSubject<string> = new BehaviorSubject<string>('tracks');
+
   changeResultType(event): void {
     this.showResultType$.next(event.target.value);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.pois != null && changes.pois.currentValue != null) {
+      this.showResultType$.next('pois');
+    }
   }
 }
