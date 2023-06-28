c0b259766713d16ae849056fce424bfc7bad4259
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2023-06-28 16:28:09 +0200

    fix(HOME_inputTyped_wm_home_result_search): implements all stories

--- home/home-result/home-result.component.ts
+++ home/home-result/home-result.component.ts
@@ -1,20 +1,19 @@
-import {countAll} from './../../store/api/api.selector';
 import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
-  OnChanges,
+  OnDestroy,
   Output,
-  SimpleChanges,
   ViewEncapsulation,
 } from '@angular/core';
 import {Store} from '@ngrx/store';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, Subscription, combineLatest} from 'rxjs';
 import {filter, map} from 'rxjs/operators';
 import {
   apiElasticStateLoading,
   countPois,
   countTracks,
+  countAll,
   featureCollection,
   queryApi,
 } from '../../store/api/api.selector';
@@ -26,7 +25,7 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class WmHomeResultComponent implements OnChanges {
+export class WmHomeResultComponent implements OnDestroy {
   @Output() poiEVT: EventEmitter<any> = new EventEmitter();
   @Output() trackEVT: EventEmitter<number> = new EventEmitter();
 
@@ -40,14 +39,31 @@ export class WmHomeResultComponent implements OnChanges {
     map(p => ((p as any).features || []).map(p => (p as any).properties || [])),
   );
   tracksLoading$: Observable<boolean> = this._store.select(apiElasticStateLoading);
+  private _resultTypeSub$: Subscription = Subscription.EMPTY;
 
   changeResultType(event): void {
     this.showResultType$.next(event.target.value);
   }
-  constructor(private _store: Store) {}
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.pois != null && changes.pois.currentValue != null) {
-      this.showResultType$.next('pois');
-    }
+
+  constructor(private _store: Store) {
+    this._resultTypeSub$ = combineLatest([this.countTracks$, this.countPois$])
+      .pipe(
+        map(([tracks, pois]) => {
+          if ((tracks > 0 && pois === 0) || (tracks > 0 && pois > 0)) {
+            return 'tracks';
+          } else if (pois > 0 && tracks === 0) {
+            return 'pois';
+          } else {
+            return 'none';
+          }
+        }),
+      )
+      .subscribe(value => {
+        this.showResultType$.next(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._resultTypeSub$.unsubscribe();
   }
 }
