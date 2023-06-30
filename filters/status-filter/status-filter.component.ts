297f2b586ce9d103aafec62f52d3d58baa21d09c
bongiu <peppedeka@gmail.com>
2023-06-30 09:40:19 +0200

    fix(status-filter): count all activated filters

--- filters/status-filter/status-filter.component.ts
+++ filters/status-filter/status-filter.component.ts
@@ -1,3 +1,4 @@
+import { countAll } from './../../store/api/api.selector';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -30,22 +31,18 @@ export class StatusFilterComponent {
   @Output() removePoiFilterEVT: EventEmitter<string> = new EventEmitter();
   @Output() removeTrackFilterEVT: EventEmitter<Filter> = new EventEmitter();
   @Output() resetFiltersEVT: EventEmitter<void> = new EventEmitter();
+
+  countAll$: Observable<number> = this._store.select(countAll);
   layer$ = this._store.select(apiElasticStateLayer);
-  trackFilters$: Observable<any[]> = this._store.select(apiFilterTracks);
   poiFilters$: Observable<any> = this._store.select(poiFilters);
+  trackFilters$: Observable<any[]> = this._store.select(apiFilterTracks);
+
   constructor(private _store: Store) {}
 
-  resetFilters(): void {
+  removeLayer(layer: any): void {
     this._store.dispatch(setLayer(null));
-    this._store.dispatch(resetPoiFilters());
     this._store.dispatch(resetTrackFilters());
-    this._store.dispatch(goToHome());
-    this.resetFiltersEVT.emit();
-  }
-
-  removeTrackFilter(filter: Filter): void {
-    this._store.dispatch(toggleTrackFilter({filter}));
-    this.removeTrackFilterEVT.emit(filter);
+    this.removeLayerEVT.emit(layer);
   }
 
   removePoiFilter(filterIdentifier: string): void {
@@ -53,9 +50,16 @@ export class StatusFilterComponent {
     this.removePoiFilterEVT.emit(filterIdentifier);
   }
 
-  removeLayer(layer: any): void {
+  removeTrackFilter(filter: Filter): void {
+    this._store.dispatch(toggleTrackFilter({filter}));
+    this.removeTrackFilterEVT.emit(filter);
+  }
+
+  resetFilters(): void {
     this._store.dispatch(setLayer(null));
+    this._store.dispatch(resetPoiFilters());
     this._store.dispatch(resetTrackFilters());
-    this.removeLayerEVT.emit(layer);
+    this._store.dispatch(goToHome());
+    this.resetFiltersEVT.emit();
   }
 }
