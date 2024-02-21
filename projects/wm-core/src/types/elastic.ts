caed57f81e6c1f59252453f285a6cb1cc9a0c9ac
peppedeka <peppedeka@gmail.com>
2024-02-21 12:00:03 +0100

    chore: Update search-box.component.html and elastic.ts
    - In search-box.component.html, added a null check for data.properties.name before displaying it.
    - In elastic.ts, added a properties field to the IHIT interface.

--- projects/wm-core/src/types/elastic.ts
+++ projects/wm-core/src/types/elastic.ts
@@ -1,6 +1,8 @@
+
 export interface IELASTIC {
   [name: string]: any;
 }
+
 export interface IHIT {
   cai_scale: string;
   distance: string;
@@ -8,6 +10,8 @@ export interface IHIT {
   id: number;
   layers: number[];
   name: string;
+  properties: {[key:string]:any}
+
   ref: string;
   size?: any;
   taxonomyActivities: any;
