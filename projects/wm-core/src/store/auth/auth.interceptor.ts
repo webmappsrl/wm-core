519cff7ca311f5d0076411cb457fd8b9146958f4
bongiu <peppedeka@gmail.com>
2024-09-06 11:38:56 +0200

    chore: Update auth.interceptor.ts
    - Added import for EnvironmentConfig and ENVIRONMENT_CONFIG
    - Injected EnvironmentConfig in the constructor
    - Modified intercept method to include check for Geohub URL and add new headers if idToken is present

--- projects/wm-core/src/store/auth/auth.interceptor.ts
+++ projects/wm-core/src/store/auth/auth.interceptor.ts
@@ -1,21 +1,26 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
+import { ENVIRONMENT_CONFIG, EnvironmentConfig } from '../conf/conf.token';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(@Inject(ENVIRONMENT_CONFIG) public environment: EnvironmentConfig,) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const idToken = localStorage.getItem('access_token');
-
-    if (idToken) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', 'Bearer ' + idToken),
+    const isGeohubUrl =req.url.includes(this.environment.api);
+    if (idToken && isGeohubUrl) {
+      // Cloniamo la richiesta per aggiungere le nuove intestazioni
+      const clonedReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${idToken}`,
+          'App-id': `${this.environment.geohubId}`,
+        },
       });
 
-      return next.handle(cloned);
+      return next.handle(clonedReq);
     } else {
       return next.handle(req);
     }
