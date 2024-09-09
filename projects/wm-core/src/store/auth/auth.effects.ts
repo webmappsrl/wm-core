5a578bb88e8d26249dfac565e1f7a6976f69d5cb
bongiu <peppedeka@gmail.com>
2024-09-09 14:15:50 +0200

    chore: Update auth.effects.ts
    - Added import statement for SaveService
    - Renamed loadSignup$ effect to deleteUser$
    - Updated ofType() in deleteUser$ effect to AuthActions.deleteUser
    - Updated map() and catchError() in deleteUser$ effect to match the new action names (deleteUserSuccess and deleteUserFailure)
    - Added loadAuth$ effect to handle loading user authentication data
    - Updated ofType() in loadAuth$ effect to AuthActions.loadAuths
    - Updated map() and catchError() in loadAuth$ effect to match the new action names (loadAuthsSuccess and loadAuthsFailure)
    - Renamed loadSignin$ effect to loadSignups$
    - Updated ofType() in loadSignup$ effect to AuthActions.loadSignUps
    - Updated map() and catchError() in loadSignup$ effect to match the new action names (loadSignUpsSuccess and loadSignUpsFailure)
    - Removed unused openAlertSignInSuccess$, openAlertOnError$, and openAlertSignOutSuccess$ effects

--- projects/wm-core/src/store/auth/auth.effects.ts
+++ projects/wm-core/src/store/auth/auth.effects.ts
@@ -6,73 +6,10 @@ import {AuthService} from './auth.service';
 import {of} from 'rxjs';
 import {AlertController, NavController} from '@ionic/angular';
 import {LangService} from 'wm-core/localization/lang.service';
+import { SaveService } from 'wm-core/services/save.service';
 
 @Injectable()
 export class AuthEffects {
-  loadSignup$ = createEffect(() => {
-    return this._actions$.pipe(
-      ofType(AuthActions.loadSignUps),
-      switchMap(action =>
-        this._authSvc.signUp(action.name, action.email, action.password, action.referrer).pipe(
-          map(user => {
-            return AuthActions.loadSignUpsSuccess({user});
-          }),
-          catchError(error => {
-            return of(AuthActions.loadSignUpsFailure({error}));
-          }),
-        ),
-      ),
-    );
-  });
-
-  loadSignin$ = createEffect(() => {
-    return this._actions$.pipe(
-      ofType(AuthActions.loadSignIns),
-      switchMap(action =>
-        this._authSvc.login(action.email, action.password).pipe(
-          map(user => {
-            return AuthActions.loadSignInsSuccess({user});
-          }),
-          catchError(error => {
-            return of(AuthActions.loadSignInsFailure({error}));
-          }),
-        ),
-      ),
-    );
-  });
-
-  loadAuth$ = createEffect(() => {
-    return this._actions$.pipe(
-      ofType(AuthActions.loadAuths),
-      switchMap(action =>
-        this._authSvc.getUser().pipe(
-          map(user => {
-            return AuthActions.loadAuthsSuccess({user});
-          }),
-          catchError(error => {
-            return of(AuthActions.loadAuthsFailure({error}));
-          }),
-        ),
-      ),
-    );
-  });
-
-  logout$ = createEffect(() => {
-    return this._actions$.pipe(
-      ofType(AuthActions.loadSignOuts),
-      switchMap(action =>
-        this._authSvc.logout().pipe(
-          map(_ => {
-            return AuthActions.loadSignOutsSuccess();
-          }),
-          catchError(error => {
-            return of(AuthActions.loadSignOutsFailure({error}));
-          }),
-        ),
-      ),
-    );
-  });
-
   deleteUser$ = createEffect(() => {
     return this._actions$.pipe(
       ofType(AuthActions.deleteUser),
@@ -88,55 +25,66 @@ export class AuthEffects {
       ),
     );
   });
-
-  openAlertSignOutSuccess$ = createEffect(
-    () =>
-      this._actions$.pipe(
-        ofType(AuthActions.loadSignOutsSuccess),
-        switchMap(() =>
-          this._alertCtrl.create({
-            mode: 'ios',
-            header: this._langSvc.instant('Logout effettuato con successo'),
-            message: '',
-            buttons: [
-              {
-                text: 'ok',
-              },
-            ],
+  loadAuth$ = createEffect(() => {
+    return this._actions$.pipe(
+      ofType(AuthActions.loadAuths),
+      switchMap(action =>
+        this._authSvc.getUser().pipe(
+          map(user => {
+            return AuthActions.loadAuthsSuccess({user});
+          }),
+          catchError(error => {
+            return of(AuthActions.loadAuthsFailure({error}));
           }),
         ),
-        switchMap(alert => {
-          alert.present();
-          return alert.onWillDismiss();
-        }),
       ),
-    {dispatch: false},
-  );
-
-  openAlertSignInSuccess$ = createEffect(
-    () =>
-      this._actions$.pipe(
-        ofType(AuthActions.loadSignInsSuccess),
-        switchMap(() =>
-          this._alertCtrl.create({
-            mode: 'ios',
-            header: this._langSvc.instant('Login effettuato con successo'),
-            message: '',
-            buttons: [
-              {
-                text: 'ok',
-              },
-            ],
+    );
+  });
+  loadSignin$ = createEffect(() => {
+    return this._actions$.pipe(
+      ofType(AuthActions.loadSignIns),
+      switchMap(action =>
+        this._authSvc.login(action.email, action.password).pipe(
+          map(user => {
+            return AuthActions.loadSignInsSuccess({user});
+          }),
+          catchError(error => {
+            return of(AuthActions.loadSignInsFailure({error}));
           }),
         ),
-        switchMap(alert => {
-          alert.present();
-          return alert.onWillDismiss();
-        }),
       ),
-    {dispatch: false},
-  );
-
+    );
+  });
+  loadSignup$ = createEffect(() => {
+    return this._actions$.pipe(
+      ofType(AuthActions.loadSignUps),
+      switchMap(action =>
+        this._authSvc.signUp(action.name, action.email, action.password, action.referrer).pipe(
+          map(user => {
+            return AuthActions.loadSignUpsSuccess({user});
+          }),
+          catchError(error => {
+            return of(AuthActions.loadSignUpsFailure({error}));
+          }),
+        ),
+      ),
+    );
+  });
+  logout$ = createEffect(() => {
+    return this._actions$.pipe(
+      ofType(AuthActions.loadSignOuts),
+      switchMap(action =>
+        this._authSvc.logout().pipe(
+          map(_ => {
+            return AuthActions.loadSignOutsSuccess();
+          }),
+          catchError(error => {
+            return of(AuthActions.loadSignOutsFailure({error}));
+          }),
+        ),
+      ),
+    );
+  });
   openAlertOnError$ = createEffect(
     () =>
       this._actions$.pipe(
@@ -166,12 +114,62 @@ export class AuthEffects {
       ),
     {dispatch: false},
   );
+  openAlertSignInSuccess$ = createEffect(
+    () =>
+      this._actions$.pipe(
+        ofType(AuthActions.loadSignInsSuccess),
+        switchMap(() => {
+          return this._alertCtrl.create({
+            mode: 'ios',
+            header: this._langSvc.instant('Login effettuato con successo'),
+            message: '',
+            buttons: [
+              {
+                text: 'ok',
+              },
+            ],
+          });
+        }),
+        switchMap(alert => {
+          alert.present();
+          setTimeout(() => {
+            this._saveSvc.syncUgc();
+          }, 2000);
+
+          return alert.onWillDismiss();
+        }),
+      ),
+    {dispatch: false},
+  );
+  openAlertSignOutSuccess$ = createEffect(
+    () =>
+      this._actions$.pipe(
+        ofType(AuthActions.loadSignOutsSuccess),
+        switchMap(() =>
+          this._alertCtrl.create({
+            mode: 'ios',
+            header: this._langSvc.instant('Logout effettuato con successo'),
+            message: '',
+            buttons: [
+              {
+                text: 'ok',
+              },
+            ],
+          }),
+        ),
+        switchMap(alert => {
+          alert.present();
+          return alert.onWillDismiss();
+        }),
+      ),
+    {dispatch: false},
+  );
 
   constructor(
     private _actions$: Actions,
     private _authSvc: AuthService,
-    private _navCtrl: NavController,
     private _alertCtrl: AlertController,
     private _langSvc: LangService,
+    private _saveSvc: SaveService,
   ) {}
 }
