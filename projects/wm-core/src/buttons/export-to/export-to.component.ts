b6409b38b7357b8dee06df45ef29ff01adbcbb4e
bongiu <peppedeka@gmail.com>
2024-09-13 11:43:24 +0200

    chore: Update export-to.component.ts and track-download-urls.component.ts
    - Added imports for Filesystem, Directory, Encoding, AlertController, and Share
    - Added DeviceService dependency injection
    - Added mobileSave() method to save files in the filesystem on mobile devices
    - Added requestStoragePermission() method to request storage permission on mobile devices
    - Updated save() method to use webSave() or mobileSave() based on the device type
    - Added showSuccessPopup() method to display a success popup with options to share the saved file
    - Updated webSave() method to remove spaces from the file name before downloading
    chore: Update export-to.component.ts and track-download-urls.component.ts
    
    - Added imports for Filesystem, Directory, Encoding, AlertController, and Share
    - Added DeviceService dependency injection
    - Added mobileSave() method to save files in the filesystem on mobile devices
    - Added requestStoragePermission() method to request storage permission on mobile devices
    - Updated save() method to use webSave() or mobileSave() based on the device type
    - Added showSuccessPopup() method to display a success popup with options to share the saved file
    - Updated webSave() method to remove spaces from the file name before downloading

--- projects/wm-core/src/buttons/export-to/export-to.component.ts
+++ projects/wm-core/src/buttons/export-to/export-to.component.ts
@@ -1,7 +1,11 @@
 import {ChangeDetectionStrategy, Component, Input, ViewEncapsulation} from '@angular/core';
 import GeoJsonToGpx from '@dwayneparton/geojson-to-gpx';
+import {Filesystem, Directory, Encoding} from '@capacitor/filesystem';
+import {AlertController} from '@ionic/angular';
 import tokml from 'geojson-to-kml';
+import {DeviceService} from 'wm-core/services/device.service';
 import {WmLoadingService} from 'wm-core/services/loading.service';
+import {Share} from '@capacitor/share';
 
 @Component({
   selector: 'wm-export-to-btn',
@@ -12,27 +16,24 @@ import {WmLoadingService} from 'wm-core/services/loading.service';
 })
 export class ExportToBtnComponent {
   @Input() input: any;
-  @Input() saveFileFn = (data: string, format: string, input: any): void => {
-    const blob = new Blob([data], {type: 'text/plain'});
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `export.${format}`;
-    a.click();
-    window.URL.revokeObjectURL(url);
-  };
   @Input() to: 'gpx' | 'kml' | 'geojson' | 'json' = 'gpx';
 
-  constructor(private _loadingSvc: WmLoadingService) {}
+  constructor(
+    private _loadingSvc: WmLoadingService,
+    private _deviceSvc: DeviceService,
+    private _alertCtrl: AlertController,
+  ) {
+  }
 
   export(): void {
     this._loadingSvc.show(`build ${this.to} file`);
     let output;
+    let g;
     try {
-    if (this.input == null && this.input.geojson == null) {
-      throw new Error('no data to export');
-    }
-      const g = this._toGeoJSON(this.input.geojson);
+      if (this.input == null && this.input.geojson == null) {
+        throw new Error('no data to export');
+      }
+      g = this._toGeoJSON(this.input.geojson);
       g.properties = {...g.properties, ...this.input.rawData, ...{title: this.input.title}};
       switch (this.to) {
         case 'gpx':
@@ -63,7 +64,82 @@ export class ExportToBtnComponent {
       this._loadingSvc.close(`build ${this.to} file`);
     }
     this._loadingSvc.close(`build ${this.to} file`);
-    this.saveFileFn(output, this.to, this.input);
+    this.save(output, this.to, g.properties);
+  }
+
+  async mobileSave(data, format, name): Promise<void> {
+    const fileName = `${name}.${format}`;
+    try {
+      // Scrivi il file nel filesystem
+      const writeResult = await Filesystem.writeFile({
+        path: `${fileName}`,
+        data,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      });
+
+      // Prepara il file per la condivisione
+      const fileUrl = writeResult.uri;
+
+      // Mostra il popup con il messaggio e la richiesta di condivisione
+      await this.showSuccessPopup(fileName, fileUrl);
+    } catch (e) {
+      console.error("Errore durante l'esportazione e la condivisione:", e);
+    }
+  }
+
+  save(data, format, properties): void {
+    const name = this._getName(properties.name);
+    this._deviceSvc.isBrowser ? this.webSave(data, format,name) : this.mobileSave(data, format, name);
+  }
+
+  async showSuccessPopup(fileName: string, fileUrl: string): Promise<void> {
+    const alert = await this._alertCtrl.create({
+      header: 'File salvato',
+      message: `File correttamente salvato in Documenti come ${fileName} Vuoi condividerlo?`,
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            console.log('Condivisione annullata');
+          },
+        },
+        {
+          text: 'Sì',
+          handler: async () => {
+            await Share.share({
+              title: `Condividi il file ${fileName}`,
+              url: fileUrl,
+              dialogTitle: `Condividi il tuo file ${fileName}`,
+            });
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  webSave(data: string, format: any, name:string): void {
+    const blob = new Blob([data], {type: 'text/plain'});
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${name}.${format}`;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  private _getName(name: string | { [keys: string]: string | undefined }): string {
+    if (name == null) {
+      return 'export';
+    }
+    if (typeof name === 'string') {
+      return name.replace(/\s+/g, '');  // Rimuove tutti gli spazi
+    }
+    const values = Object.values(name);
+    return values[0] ? values[0].replace(/\s+/g, '') : 'export';  // Rimuove spazi dal primo valore non undefined
   }
 
   private _toGeoJSON(obj): any {
--- projects/wm-core/src/track-download-urls/track-download-urls.component.ts
+++ projects/wm-core/src/track-download-urls/track-download-urls.component.ts
@@ -2,9 +2,11 @@ import {Component, Input} from '@angular/core';
 import {Filesystem, Directory, Encoding} from '@capacitor/filesystem';
 import {Share} from '@capacitor/share';
 import GeoJsonToGpx from '@dwayneparton/geojson-to-gpx';
+import { AlertController } from '@ionic/angular';
 import tokml from 'geojson-to-kml';
-import { keys } from 'localforage';
 import { DeviceService } from 'wm-core/services/device.service';
+import {Plugins} from '@capacitor/core';
+const {Permissions} = Plugins;
 @Component({
   selector: 'wm-track-download-urls',
   templateUrl: './track-download-urls.component.html',
@@ -25,7 +27,9 @@ export class WmTrackDownloadUrlsComponent {
   kml: string;
   osm: string;
 
-  constructor(private _deviceSvc:DeviceService) {}
+  constructor(private _deviceSvc:DeviceService,private _alertCtrl: AlertController) {
+    this.requestStoragePermission();
+  }
 
   catch (e) {
       console.error(e);
@@ -61,34 +65,69 @@ export class WmTrackDownloadUrlsComponent {
     }
 
   async mobileSave(data, format): Promise<void> {
-    const name = this._getName(this._properties?.name);
-    const fileName = `${name}.${format}`;
-    try {
-      // Scrivi il file nel filesystem
-      const writeResult = await Filesystem.writeFile({
-        path: fileName,
-        data,
-        directory: Directory.External,
-        encoding: Encoding.UTF8,
-      });
+      const name = this._getName(this._properties?.name);
+      const fileName = `${name}.${format}`;
+      try {
+        // Scrivi il file nel filesystem
+        const writeResult = await Filesystem.writeFile({
+          path: fileName,
+          data,
+          directory: Directory.Documents,
+          encoding: Encoding.UTF8,
+        });
+  
+        // Prepara il file per la condivisione
+        const fileUrl = writeResult.uri;
+  
+        // Mostra il popup con il messaggio e la richiesta di condivisione
+        await this.showSuccessPopup(fileName, fileUrl);
+      } catch (e) {
+        console.error('Errore durante l\'esportazione e la condivisione:', e);
+      }
+    }
 
-      // Prepara il file per la condivisione
-      const fileUrl = writeResult.uri;
+  async requestStoragePermission() {
+    const permission = await Permissions.query({name: 'storage'});
 
-      // Apri il menu di condivisione
-      await Share.share({
-        title: `Condividi il file ${fileName}`,
-        url: fileUrl,
-        dialogTitle: `Condividi il tuo file ${fileName}`,
-      });
-    } catch (e) {
-      console.error("Errore durante l'esportazione e la condivisione:", e);
+    if (permission.state !== 'granted') {
+      const result = await Permissions.request({name: 'storage'});
+      if (result.state !== 'granted') {
+        throw new Error('Permesso di archiviazione non concesso');
+      }
     }
   }
 
   save(data,format):void {
     this._deviceSvc.isBrowser ? this.webSave(data, format):this.mobileSave(data, format);
   }
+
+  async showSuccessPopup(fileName: string, fileUrl: string): Promise<void> {
+      const alert = await this._alertCtrl.create({
+        header: 'File salvato',
+        message: `File correttamente salvato in Documenti come ${fileName} Vuoi condividerlo?`,
+        buttons: [
+          {
+            text: 'No',
+            role: 'cancel',
+            handler: () => {
+              console.log('Condivisione annullata');
+            }
+          },
+          {
+            text: 'Sì',
+            handler: async () => {
+              await Share.share({
+                title: `Condividi il file ${fileName}`,
+                url: fileUrl,
+                dialogTitle: `Condividi il tuo file ${fileName}`,
+              });
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+    }
 
   webSave(data: string, format: any): void  {
     const blob = new Blob([data], {type: 'text/plain'});
@@ -101,16 +140,16 @@ export class WmTrackDownloadUrlsComponent {
     window.URL.revokeObjectURL(url);
   }
 
-  private _getName(name:string|{[keys:string]:string|undefined}):string {
-  if(name == null) {
-    return 'export';
+  private _getName(name: string | { [keys: string]: string | undefined }): string {
+    if (name == null) {
+      return 'export';
+    }
+    if (typeof name === 'string') {
+      return name.replace(/\s+/g, '');  // Rimuove tutti gli spazi
+    }
+    const values = Object.values(name);
+    return values[0] ? values[0].replace(/\s+/g, '') : 'export';  // Rimuove spazi dal primo valore non undefined
   }
-  if(typeof name === 'string') {
-    return name;
-  }
-  const values = Object.values(name);
-  return values[0];
-}
 
   private _initializeDownloadUrls(): void {
     this.gpx = this._properties?.gpx_url;
