59bbcf48dd4d1738b11100368bd90c7382ede274
bongiu <peppedeka@gmail.com>
2024-09-24 11:29:02 +0200

    chore: Update track-download-urls component
    - Removed unnecessary [button] attribute from ion-item in track-download-urls.component.html
    - Removed unused import and isUgcSelected$ variable in track-download-urls.component.ts

--- projects/wm-core/src/track-download-urls/track-download-urls.component.ts
+++ projects/wm-core/src/track-download-urls/track-download-urls.component.ts
@@ -4,9 +4,7 @@ import {Share} from '@capacitor/share';
 import GeoJsonToGpx from '@dwayneparton/geojson-to-gpx';
 import {Feature} from 'geojson';
 import tokml from 'geojson-to-kml';
-import { Observable } from 'rxjs';
 import {DeviceService} from 'wm-core/services/device.service';
-import { isUgcSelected } from 'wm-core/store/api/api.selector';
 @Component({
   selector: 'wm-track-download-urls',
   templateUrl: './track-download-urls.component.html',
@@ -14,18 +12,14 @@ import { isUgcSelected } from 'wm-core/store/api/api.selector';
 })
 export class WmTrackDownloadUrlsComponent implements OnInit {
   @Input() track: Feature;
-  isUgcSelected$: Observable<boolean> = this._store.select(isUgcSelected);
 
   osm: string;
 
+  constructor(private _deviceSvc: DeviceService) {}
 
   ngOnInit(): void {
     this.osm = this.track.properties?.osm_url;
   }
-  constructor(
-    private _deviceSvc: DeviceService,
-    private _store: Store
-  ) {}
 
   export(to: string): void {
     let output;
