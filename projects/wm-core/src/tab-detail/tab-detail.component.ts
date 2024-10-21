5ee014b3e170b487adea3e84ea58ae188a6b0ad7
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-10-21 14:36:01 +0200

    chore: Update subproject configuration options for tab details oc: 4142
    - Added new display options for distance, duration, elevation, and downloads in the tab detail component.
    - Updated the configuration interface to include these new display options.

--- projects/wm-core/src/tab-detail/tab-detail.component.ts
+++ projects/wm-core/src/tab-detail/tab-detail.component.ts
@@ -5,10 +5,15 @@ import {
   Input,
   Output,
   ViewEncapsulation,
+  OnInit
 } from '@angular/core';
 import {GeoJsonProperties} from 'geojson';
 import {IGeojsonFeature, IGeojsonProperties} from '../types/model';
 import {ISlopeChartHoverElements} from '../types/slope-chart';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {ICONF} from 'wm-core/types/config';
+import {IConfRootState} from '../store/conf/conf.reducer';
 
 @Component({
   selector: 'wm-tab-detail',
@@ -17,16 +22,21 @@ import {ISlopeChartHoverElements} from '../types/slope-chart';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class WmTabDetailComponent {
+export class WmTabDetailComponent implements OnInit {
   @Input()
   properties: GeoJsonProperties;
   @Output('slopeChartHover')
   slopeChartHover: EventEmitter<ISlopeChartHoverElements> =
     new EventEmitter<ISlopeChartHoverElements>();
 
+  public config$: Observable<ICONF>;
   public route: IGeojsonFeature;
 
-  constructor() {}
+  constructor(private store: Store<IConfRootState>) {}
+
+  ngOnInit() {
+    this.config$ = this.store.select(state => state.conf);
+  }
 
   onLocationHover(event: ISlopeChartHoverElements) {
     this.slopeChartHover.emit(event);
