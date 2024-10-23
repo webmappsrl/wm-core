2eba01d2596dc895d195b305d161b951c505f4e4
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-10-23 10:42:34 +0200

    chore: Update subproject to use configOptions selector instead of config$ for better consistency and readability oc: 4142
    Changed the usage of `config$` to `configOptions$` in the template and component file for improved clarity.

--- projects/wm-core/src/tab-detail/tab-detail.component.ts
+++ projects/wm-core/src/tab-detail/tab-detail.component.ts
@@ -5,15 +5,13 @@ import {
   Input,
   Output,
   ViewEncapsulation,
-  OnInit
 } from '@angular/core';
 import {GeoJsonProperties} from 'geojson';
 import {IGeojsonFeature, IGeojsonProperties} from '../types/model';
 import {ISlopeChartHoverElements} from '../types/slope-chart';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
-import {ICONF} from 'wm-core/types/config';
-import {IConfRootState} from '../store/conf/conf.reducer';
+import {confOPTIONS} from 'wm-core/store/conf/conf.selector';
 
 @Component({
   selector: 'wm-tab-detail',
@@ -22,21 +20,17 @@ import {IConfRootState} from '../store/conf/conf.reducer';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class WmTabDetailComponent implements OnInit {
+export class WmTabDetailComponent {
   @Input()
   properties: GeoJsonProperties;
   @Output('slopeChartHover')
   slopeChartHover: EventEmitter<ISlopeChartHoverElements> =
     new EventEmitter<ISlopeChartHoverElements>();
 
-  public config$: Observable<ICONF>;
+  configOptions$: Observable<any> = this._store.select(confOPTIONS);
   public route: IGeojsonFeature;
 
-  constructor(private store: Store<IConfRootState>) {}
-
-  ngOnInit() {
-    this.config$ = this.store.select(state => state.conf);
-  }
+  constructor(private _store: Store<any>) {}
 
   onLocationHover(event: ISlopeChartHoverElements) {
     this.slopeChartHover.emit(event);
