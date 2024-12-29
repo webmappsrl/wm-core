d99d40bbbc1e71991c7070c3291f0bc0c59c56cb
bongiu <peppedeka@gmail.com>
2024-12-29 13:12:30 +0100

    chore: Update auth.interceptor.ts
    - Refactor import statements for ConfService and EnvironmentConfig
    - Add ConfService dependency injection in the constructor
    - Update 'isGeohubUrl' variable to use correct environment API property
    - Update 'App-id' header value to use geohubAppId property from ConfService

--- projects/wm-core/src/store/auth/auth.interceptor.ts
+++ projects/wm-core/src/store/auth/auth.interceptor.ts
@@ -1,22 +1,26 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Inject, Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import { ENVIRONMENT_CONFIG, EnvironmentConfig } from '../conf/conf.token';
+import {ConfService} from '../conf/conf.service';
+import {ENVIRONMENT_CONFIG, EnvironmentConfig} from '../conf/conf.token';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(@Inject(ENVIRONMENT_CONFIG) public environment: EnvironmentConfig,) {}
+  constructor(
+    @Inject(ENVIRONMENT_CONFIG) public environment: EnvironmentConfig,
+    private _confSvc: ConfService,
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const idToken = localStorage.getItem('access_token');
-    const isGeohubUrl =req.url.includes(this.environment.api);
+    const isGeohubUrl = req.url.includes(this.environment.api);
     if (idToken && isGeohubUrl) {
       // Cloniamo la richiesta per aggiungere le nuove intestazioni
       const clonedReq = req.clone({
         setHeaders: {
           Authorization: `Bearer ${idToken}`,
-          'App-id': `${this.environment.geohubId}`,
+          'App-id': `${this._confSvc.geohubAppId}`,
         },
       });
 
