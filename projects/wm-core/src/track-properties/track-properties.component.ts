a62b7bdf23cb57bb12a6b32d15bbcbbb3f2ae7d7
Alessandro <alessandropeci@webmapp.it>
2025-01-24 08:21:03 +0100

    chore: Remove unused method and update URL handling in TrackPropertiesComponent
    The `getFlowPopoverText` method was removed as it was no longer being used. Additionally, the URL handling in the `onLocationHover` method was updated to use the `_urlHandlerSvc.updateURL` function. This ensures that the track property is set to undefined when hovering over a location.

--- projects/wm-core/src/track-properties/track-properties.component.ts
+++ projects/wm-core/src/track-properties/track-properties.component.ts
@@ -34,17 +34,6 @@ export class TrackPropertiesComponent {
     this._urlHandlerSvc.updateURL({track: undefined});
   }
 
-  getFlowPopoverText(altitude = 0, orangeTreshold = 800, redTreshold = 1500): string {
-    const green = `<span class="green">Livello 1: tratti non interessati dall'alta quota (quota minore di ${orangeTreshold} metri)</span>`;
-    const orange = `<span class="orange">Livello 2: tratti parzialmente in alta quota (quota compresa tra ${orangeTreshold} metri e ${redTreshold} metri)</span>`;
-    const red = `<span class="red">Livello 3: in alta quota (quota superiore ${redTreshold} metri)</span>`;
-    return altitude < orangeTreshold
-      ? green
-      : altitude > orangeTreshold && altitude < redTreshold
-      ? orange
-      : red;
-  }
-
   onLocationHover(event: WmSlopeChartHoverElements): void {
     this._store.dispatch(trackElevationChartHoverElemenents({elements: event}));
   }
