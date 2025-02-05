3168de008c3a8ef176b804bb4e3d9429abd8def1
Alessandro <alessandropeci@webmapp.it>
2025-02-05 16:11:18 +0100

    chore: Remove profile records component oc: 4892
    The profile records component and its associated HTML and SCSS files have been deleted. This component was responsible for displaying a list of profile records, including the option to open photos. However, it is no longer needed and has been removed from the project.

--- projects/wm-core/src/profile/profile.module.ts
+++ projects/wm-core/src/profile/profile.module.ts
@@ -5,13 +5,11 @@ import {WmPipeModule} from '../pipes/pipe.module';
 import {ProfileAuthComponent} from './profile-auth/profile-auth.component';
 import {ProfileUserComponent} from './profile-user/profile-user.component';
 import {ProfileDataComponent} from './profile-data/profile-data.component';
-import {ProfileRecordsComponent} from './profile-records/profile-records.component';
 import {WmProfilePopupComponent} from './profile-popup/profile-popup.component';
 const components = [
   ProfileAuthComponent,
   ProfileUserComponent,
   ProfileDataComponent,
-  ProfileRecordsComponent,
   WmProfilePopupComponent,
 ];
 @NgModule({
