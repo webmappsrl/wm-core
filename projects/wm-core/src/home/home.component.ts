d46fcfdc87557502d115712e58d981cd952da9c7
bongiu <peppedeka@gmail.com>
2025-03-21 16:04:42 +0100

    chore: Update home.component.ts and user-activity.reducer.ts
    - Removed setCurrentPoi function call in home.component.ts
    - Added setMapDetailsStatus function call in home.component.ts
    - Added setMapDetailsStatus reducer in user-activity.reducer.ts

--- projects/wm-core/src/home/home.component.ts
+++ projects/wm-core/src/home/home.component.ts
@@ -43,6 +43,7 @@ import {
   inputTyped,
   openUgc,
   togglePoiFilter,
+  setMapDetailsStatus,
   toggleTrackFilterByIdentifier,
 } from '@wm-core/store/user-activity/user-activity.action';
 import {WmSearchBarComponent} from '@wm-core/search-bar/search-bar.component';
@@ -167,6 +168,7 @@ export class WmHomeComponent implements AfterContentInit {
     this._urlHandlerSvc.updateURL({layer: layer?.id ?? undefined});
     this._store.dispatch(closeUgc());
     this._store.dispatch(closeDownloads());
+    this._store.dispatch(setMapDetailsStatus({status: 'open'}));
   }
 
   setPoi(id: string | number): void {
