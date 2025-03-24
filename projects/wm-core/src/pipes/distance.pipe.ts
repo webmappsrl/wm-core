dbb9fb05ad334bb13590b2d56df22b61bb73128c
Alessandro <alessandropeci@webmapp.it>
2025-03-24 08:15:31 +0100

    chore: Update distance pipe to support 'auto' distance calculation
    The DistancePipe in the wm-core project has been updated to include support for calculating distances automatically. Previously, the transform method only accepted 'km' or 'm' as the distance parameter. Now, it also accepts 'auto'. When 'auto' is passed as the distance parameter, the pipe will check the value and determine whether it should be displayed in kilometers or meters based on its magnitude. The updated code includes conditional logic to handle this calculation and display the result accordingly.

--- projects/wm-core/src/pipes/distance.pipe.ts
+++ projects/wm-core/src/pipes/distance.pipe.ts
@@ -4,16 +4,19 @@ import {Pipe, PipeTransform} from '@angular/core';
   name: 'distance',
 })
 export class DistancePipe implements PipeTransform {
-  transform(value: number, distance: 'km' | 'm' = 'km', precision: number = 0, format: 'text' | 'html' = 'text'): string {
+  transform(value: number, distance: 'auto' | 'km' | 'm' = 'km', precision: number = 0, format: 'text' | 'html' = 'text'): string {
+    if(value != null && distance === 'auto') {
+      if(value > 1000) {
+        return format === 'html' ? `<span class="value">${(value / 1000).toFixed(precision)}</span> <span class="unit">km</span>` : `${(value / 1000).toFixed(precision)} km`;
+      } else {
+        return format === 'html' ? `<span class="value">${value.toFixed(precision)}</span> <span class="unit">m</span>` : `${value.toFixed(precision)} m`;
+      }
+    }
     if (value != null && distance === 'km') {
-      return format === 'html'
-        ? `<span class="value">${(value).toFixed(precision)}</span> <span class="unit">km</span>`
-        : `${(value).toFixed(precision)} km`;
+      return format === 'html' ? `<span class="value">${(value).toFixed(precision)}</span> <span class="unit">km</span>` : `${(value).toFixed(precision)} km`;
     }
     if (value != null && distance === 'm') {
-      return format === 'html'
-        ? `<span class="value">${(value).toFixed(precision)}</span> <span class="unit">m</span>`
-        : `${(value).toFixed(precision)} m`;
+      return format === 'html' ? `<span class="value">${(value).toFixed(precision)}</span> <span class="unit">m</span>` : `${(value).toFixed(precision)} m`;
     }
     return `${value}`;
   }
