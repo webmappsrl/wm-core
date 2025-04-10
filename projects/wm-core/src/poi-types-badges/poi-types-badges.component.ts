32f28e041756252a8bbbb94be411d05b53d3c8ac
peco11 <alessandropeci@webmapp.it>
2025-04-10 08:11:54 +0200

    chore(poi-types-badges): add component for displaying badges of POI types oc:5275 (#79)
    This commit adds a new component called PoiTypesBadgesComponent, which is responsible for displaying badges of different POI types. The component takes an input of poiTypes and dynamically generates the badges based on the provided data. Each badge consists of an icon (if available) and the name of the POI type.
    
    The component also includes styling to ensure proper layout and visual representation of the badges. It uses flexbox to arrange the badges in a row with a small gap between them. Each badge has a border, padding, and color defined to make it visually distinct.
    
    Additionally, this commit includes some minor updates to other files:
    - ec.selector.ts: Adds temporary arcoded info to currentPoiProperties selector.
    - wm-core.module.ts: Imports and declares PoiTypesBadgesComponent in the module.
    
    These changes aim to enhance the user interface by providing a clear visual representation of different POI types through badges.

---
+++ projects/wm-core/src/poi-types-badges/poi-types-badges.component.ts
@@ -0,0 +1,69 @@
+import {ChangeDetectionStrategy, Component, Input, ViewEncapsulation} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+
+@Component({
+  selector: 'wm-poi-types-badges',
+  template: `
+    <ng-container *ngIf="poiTypes as poiTypes">
+      <div *ngFor="let poiType of poiTypes">
+        <ng-container *ngIf="poiType.icon">
+          <div [innerHTML]="sanitize(poiType.icon)"></div>
+        </ng-container>
+        <span>{{poiType.name | wmtrans}}</span>
+      </div>
+    </ng-container>
+  `,
+  styles: [
+    `
+      wm-poi-types-badges {
+        position: relative;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 8px;
+        margin: var(--wm-feature-details-margin);
+
+        > div {
+          display: flex;
+          flex-direction: row;
+          border: 1px solid var(--wm-color-medium);
+          border-radius: 24px;
+          padding: 4px 8px;
+          color: var(--wm-color-medium);
+          align-items: center;
+
+          div {
+            display: flex;
+          }
+
+          svg {
+            width: 20px;
+            height: 20px;
+            margin-right: 8px;
+
+            circle {
+              fill: var(--wm-color-medium);
+            }
+          }
+
+          span {
+            color: var(--wm-color-medium);
+            font-size: 14px;
+            font-weight: 600;
+          }
+        }
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class PoiTypesBadgesComponent {
+  @Input() poiTypes: any;
+
+  constructor(private _sanitizer: DomSanitizer) {}
+
+  sanitize(html: string) {
+    return this._sanitizer.bypassSecurityTrustHtml(html);
+  }
+}
