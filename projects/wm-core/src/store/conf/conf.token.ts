d55deff2a0ae3cb35b4ee5ffcbc8dfd543a3dc90
peco11 <alessandropeci@webmapp.it>
2025-04-17 07:59:59 +0200

    chore: Update lang.service.ts and conf.token.ts (#83)
    - Added import for APP_TRANSLATION from '@wm-core/store/conf/conf.token'
    - Added declaration of appTranslation variable of type Translations
    - Added initialization of appTranslation in the constructor
    - Updated setTranslation calls to include the appTranslation
    - Added private _init() method to set translations for each language in appTranslation

--- projects/wm-core/src/store/conf/conf.token.ts
+++ projects/wm-core/src/store/conf/conf.token.ts
@@ -1,4 +1,5 @@
 import {InjectionToken} from '@angular/core';
+import {Translations} from '@wm-types/language';
 export interface EnvironmentConfig {
   api: string;
   awsApi: string;
@@ -9,3 +10,4 @@ export interface EnvironmentConfig {
   shard: string;
 }
 export const APP_VERSION = new InjectionToken<string>('APP_VERSION');
+export const APP_TRANSLATION = new InjectionToken<Translations>('APP_TRANSLATION')
