1fa04478b464c9a301846792a9fd73a6a1b0b612
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2025-06-11 12:13:53 +0200

    chore(i18n): add new translations and icons oc: 5714
    Adds new translations for activity-related terms across multiple languages.
    
    Updates the icon mapping for various activities including cycling, art and culture, and more to enhance user interface consistency.
    
    Enhances localization by including additional terms to support improved user experience.

--- projects/wm-core/src/pipes/wm-get-icn.pipe.ts
+++ projects/wm-core/src/pipes/wm-get-icn.pipe.ts
@@ -9,7 +9,7 @@ export class WmGetIcnPipe implements PipeTransform {
     if (value === 'cycling') {
       return `${outline}bike`;
     }
-    if (value === 'mtb') {
+    if (value === 'mtb' || value === 'e-bike' || value === 'touring-bike' || value === 'mountain-bike' || value === 'road-bike' ) {
       return `${outline}MTB`;
     }
     if (value === 'gravel') {
@@ -17,6 +17,30 @@ export class WmGetIcnPipe implements PipeTransform {
     }
     if (value === 'road-bike') {
       return 'icon-vc-road-bike';
+    }
+    if (value === 'art-and-culture') {
+      return 'icon-temples';
+    }
+    if (value === 'tourism') {
+      return 'icon-star';
+    }
+     if (value === 'train-trekking') {
+      return 'icon-train';
+    }
+    if (value === 'ciaspole') {
+      return 'icon-outline-nordic-walking';
+    }
+    if (value === 'adventure') {
+      return 'icon-fill-trekking';
+    }
+    if (value === 'swimming-snorkeling') {
+      return 'icon-swimming';
+    }
+    if (value === 'birdwatching') {
+      return 'icon-binoculars';
+    }
+    if (value === 'oeno-gastronomy') {
+      return 'icon-restaurant';
     }
     return `${outline}${value}`;
   }
