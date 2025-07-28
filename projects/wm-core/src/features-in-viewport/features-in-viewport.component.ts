69d64bb3edab557b3cbfc8c59af12a25e7ae32c5
Alessandro <alessandropeci@webmapp.it>
2025-07-28 10:46:12 +0200

    refactor(features-in-viewport): 🔄 make sliderOptions observable
    Changed sliderOptions to an observable sliderOptions$ to dynamically adjust slide settings based on the number of features in the viewport. This allows for different configurations when there is only one feature compared to multiple features. The adjustment includes changes to properties like `slidesPerView`, `spaceBetween`, `slidesOffsetBefore`, `slidesOffsetAfter`, and `centeredSlides`.

--- projects/wm-core/src/features-in-viewport/features-in-viewport.component.ts
+++ projects/wm-core/src/features-in-viewport/features-in-viewport.component.ts
@@ -3,6 +3,7 @@ import {Store} from '@ngrx/store';
 import {UrlHandlerService} from '@wm-core/services/url-handler.service';
 import {featuresInViewport} from '@wm-core/store/user-activity/user-activity.selector';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'wm-features-in-viewport',
@@ -13,15 +14,33 @@ import {Observable} from 'rxjs';
 })
 export class WmFeaturesInViewportComponent {
   featuresInViewport$: Observable<any[]> = this._store.select(featuresInViewport);
-  sliderOptions: any = {
-    slidesPerView: 1.2,
-    spaceBetween: 16,
-    slidesOffsetBefore: 16,
-    slidesOffsetAfter: 16,
-    freeMode: true,
-  };
+  sliderOptions$: Observable<any>;
 
-  constructor(private _store: Store, private _urlHandlerSvc: UrlHandlerService) {}
+  constructor(private _store: Store, private _urlHandlerSvc: UrlHandlerService) {
+    this.sliderOptions$ = this.featuresInViewport$.pipe(
+      map(features => {
+        if (features.length === 1) {
+          return {
+            slidesPerView: 1.2,
+            spaceBetween: 0,
+            slidesOffsetBefore: 0,
+            slidesOffsetAfter: 0,
+            centeredSlides: true,
+            freeMode: true,
+          };
+        } else {
+          return {
+            slidesPerView: 1.2,
+            spaceBetween: 16,
+            slidesOffsetBefore: 16,
+            slidesOffsetAfter: 16,
+            centeredSlides: false,
+            freeMode: true,
+          };
+        }
+      })
+    );
+  }
 
   setTrack(id: number): void {
     this._urlHandlerSvc.setTrack(id);
