db8c1df16b964f0ce64bea37a69b32fe8d925cca
Alessandro <alessandropeci@webmapp.it>
2025-09-08 15:20:46 +0200

    refactor(track-related-poi): ♻️ update icon rendering logic
    Replaced the existing SVG rendering logic with a more efficient approach that uses an observable to fetch icons asynchronously. This change ensures that icon retrieval is streamlined and reduces unnecessary checks, improving the component's performance and maintainability. Removed the redundant usage of `startWith` in the RxJS pipeline and integrated the new `icons$` observable to manage icon states.

--- projects/wm-core/src/track-related-poi/track-related-poi.component.ts
+++ projects/wm-core/src/track-related-poi/track-related-poi.component.ts
@@ -16,7 +16,9 @@ import {
 import {Observable, BehaviorSubject, combineLatest, of} from 'rxjs';
 import {UrlHandlerService} from '@wm-core/services/url-handler.service';
 import {GeolocationService} from '@wm-core/services/geolocation.service';
-import {map, startWith, switchMap} from 'rxjs/operators';
+import {map, switchMap} from 'rxjs/operators';
+import {ICONS} from '@wm-types/config';
+import {icons} from '@wm-core/store/icons/icons.selector';
 
 export const MAX_VISIBLE_POIS = 4;
 @Component({
@@ -35,6 +37,7 @@ export class TrackRelatedPoiComponent {
   currentRelatedEcPid$ = this._store.select(currentEcRelatedPoiId);
   defaultPhotoPath = '/assets/icon/no-photo.svg';
   isExpanded$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  icons$: Observable<ICONS> = this._store.select(icons);
   pois$: Observable<WmFeature<Point>[]> = this._store.select(currentEcRelatedPois).pipe(
     switchMap(pois =>
       pois?.length ? combineLatest([
