5b4bea01ab9c005071544ece0606b222dc9365db
peppedeka <peppedeka@gmail.com>
2026-01-27 15:05:14 +0100

    feat(initialization): ✨ enhance environment and posthog configuration
    - Replaced inline `EnvironmentConfig` interface with imported `Environment` type from `@wm-types/environment`.
    - Introduced `ENVIRONMENT_CONFIG` token for dependency injection of environment configurations.
    - Added initialization logic for `EnvironmentService` and `PosthogCapacitorClient` in `WmCoreModule`.
    - Implemented thorough validation and logging for PostHog properties initialization.
    - Introduced `forRoot` method to `WmCoreModule` for module configuration with `appVersion`, `environment`, `posthog`, and optional `translations`.
    - Ensured app initialization robustness by handling errors gracefully during initialization.

--- projects/wm-core/src/store/conf/conf.token.ts
+++ projects/wm-core/src/store/conf/conf.token.ts
@@ -1,16 +1,10 @@
 import {InjectionToken} from '@angular/core';
 import {WmTranslations} from '@wm-types/language';
 import {WmPosthogClient, WmPosthogConfig} from '@wm-types/posthog';
-export interface EnvironmentConfig {
-  api: string;
-  awsApi: string;
-  elasticApi: string;
-  geohubId: number;
-  graphhopperHost: string;
-  production: boolean;
-  shard: string;
-}
+import {Environment} from '@wm-types/environment';
+
 export const APP_VERSION = new InjectionToken<string>('APP_VERSION');
 export const APP_TRANSLATION = new InjectionToken<WmTranslations>('APP_TRANSLATION');
 export const POSTHOG_CLIENT = new InjectionToken<WmPosthogClient>('POSTHOG_CLIENT');
 export const POSTHOG_CONFIG = new InjectionToken<WmPosthogConfig>('POSTHOG_CONFIG');
+export const ENVIRONMENT_CONFIG = new InjectionToken<Environment>('ENVIRONMENT_CONFIG');
