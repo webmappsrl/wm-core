012e1afb10a2c55ff0d7b7e3dfed44851dfbdf18
peco11 <alessandropeci@webmapp.it>
2026-02-23 14:05:24 +0100

    style(features-in-viewport): ✨ enhance styling and responsiveness (#155)
    Updated the features-in-viewport component's SCSS to include transition effects, improved padding for the search box, and added user-select properties to images. Adjusted the layout for desktop view when the details container is visible, ensuring better responsiveness. Modified TypeScript to integrate device detection for slider options, enhancing the user experience on mobile devices.

--- projects/wm-core/src/features-in-viewport/features-in-viewport.component.ts
+++ projects/wm-core/src/features-in-viewport/features-in-viewport.component.ts
@@ -1,5 +1,6 @@
 import {Component, ChangeDetectionStrategy, ViewEncapsulation} from '@angular/core';
 import {Store} from '@ngrx/store';
+import { DeviceService } from '@wm-core/services/device.service';
 import {UrlHandlerService} from '@wm-core/services/url-handler.service';
 import {featuresInViewport} from '@wm-core/store/user-activity/user-activity.selector';
 import {Observable} from 'rxjs';
@@ -16,7 +17,7 @@ export class WmFeaturesInViewportComponent {
   featuresInViewport$: Observable<any[]> = this._store.select(featuresInViewport);
   sliderOptions$: Observable<any>;
 
-  constructor(private _store: Store, private _urlHandlerSvc: UrlHandlerService) {
+  constructor(private _store: Store, private _urlHandlerSvc: UrlHandlerService, private _deviceSvc: DeviceService) {
     this.sliderOptions$ = this.featuresInViewport$.pipe(
       map(features => this._getSliderOptions(features.length))
     );
@@ -28,9 +29,10 @@ export class WmFeaturesInViewportComponent {
 
   private _getSliderOptions(featureCount: number): any {
     const isSingleFeature = featureCount === 1;
+    const isMobile = this._deviceSvc.isMobile;
     return {
-      slidesPerView: 1.2,
-      spaceBetween: isSingleFeature ? 0 : 16,
+      slidesPerView: isMobile ? 1.2 : 2.5,
+      spaceBetween: isSingleFeature ? 0 : 8,
       slidesOffsetBefore: isSingleFeature ? 0 : 16,
       slidesOffsetAfter: isSingleFeature ? 0 : 16,
       centeredSlides: isSingleFeature,
