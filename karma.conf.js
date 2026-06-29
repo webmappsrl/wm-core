7696b91500558698e059d92eb8b0b6312f4166bf
Alessandro <alessandropeci@webmapp.it>
2026-06-29 12:33:51 +0200

    fix: update Karma configuration and improve PosthogCapacitorClient tests
    - Updated ChromeHeadlessNoSandbox launcher in karma.conf.js to include '--disable-dev-shm-usage' flag for better CI compatibility.
    - Modified PosthogCapacitorClient tests to cast event properties to 'any' type, ensuring compatibility with TypeScript's strict type checking.

--- karma.conf.js
+++ karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
       }
     },
     singleRun: !!isCI,
